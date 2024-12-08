dbe73d7d0ece919fcdf52d6c3fe4c2927d72e6bb
Hyunsu Cho <phcho@nvidia.com>
2024-12-07 18:55:38 -0800

    Set up CI/CD pipeline for CI containers

---
+++ containers/docker_build.py
@@ -0,0 +1,140 @@
+"""
+Wrapper script to build a Docker container
+"""
+
+import argparse
+import itertools
+import pathlib
+import subprocess
+import sys
+import textwrap
+
+PROJECT_ROOT_DIR = pathlib.Path(__file__).parent.parent
+LINEWIDTH = 88
+TEXT_WRAPPER = textwrap.TextWrapper(
+    width=LINEWIDTH,
+    initial_indent="",
+    subsequent_indent="    ",
+    break_long_words=False,
+    break_on_hyphens=False,
+)
+
+
+def fancy_print_cli_args(cli_args: list[str]) -> None:
+    print(
+        "=" * LINEWIDTH
+        + "\n"
+        + "  \\\n".join(TEXT_WRAPPER.wrap(" ".join(cli_args)))
+        + "\n"
+        + "=" * LINEWIDTH
+        + "\n",
+        flush=True,
+    )
+
+
+def parse_build_args(*, raw_build_args: list[str]) -> dict[str, str]:
+    parsed_build_args = dict()
+    for arg in raw_build_args:
+        try:
+            key, value = arg.split("=", maxsplit=1)
+        except ValueError as e:
+            raise ValueError(
+                f"Build argument must be of form KEY=VALUE. Got: {arg}"
+            ) from e
+        parsed_build_args[key] = value
+    return parsed_build_args
+
+
+def docker_build(
+    *,
+    container_tag: str,
+    build_args: dict[str, str],
+    dockerfile_path: pathlib.Path,
+    docker_context_path: pathlib.Path,
+) -> None:
+    ## Set up command-line arguments to be passed to `docker build`
+    # Build args
+    docker_build_cli_args = list(
+        itertools.chain.from_iterable(
+            [["--build-arg", f"{k}={v}"] for k, v in build_args.items()]
+        )
+    )
+    # When building an image using a non-default driver, we need to specify
+    # `--load` to load it to the image store.
+    # See https://docs.docker.com/build/builders/drivers/
+    docker_build_cli_args.append("--load")
+    # Remaining CLI args
+    docker_build_cli_args.extend(
+        [
+            "--progress=plain",
+            "--ulimit",
+            "nofile=1024000:1024000",
+            "-t",
+            container_tag,
+            "-f",
+            str(dockerfile_path),
+            str(docker_context_path),
+        ]
+    )
+    cli_args = ["docker", "build"] + docker_build_cli_args
+    fancy_print_cli_args(cli_args)
+    subprocess.run(cli_args, check=True, encoding="utf-8")
+
+
+def main(*, args: argparse.Namespace) -> None:
+    docker_context_path = PROJECT_ROOT_DIR / "containers"
+    dockerfile_path = (
+        docker_context_path / "dockerfile" / f"Dockerfile.{args.container_def}"
+    )
+
+    build_args = parse_build_args(raw_build_args=args.build_arg)
+
+    docker_build(
+        container_tag=args.container_tag,
+        build_args=build_args,
+        dockerfile_path=dockerfile_path,
+        docker_context_path=docker_context_path,
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Build a Docker container")
+    parser.add_argument(
+        "--container-def",
+        type=str,
+        required=True,
+        help=(
+            "String uniquely identifying the container definition. The container "
+            "definition will be fetched from "
+            "containers/dockerfile/Dockerfile.CONTAINER_DEF."
+        ),
+    )
+    parser.add_argument(
+        "--container-tag",
+        type=str,
+        required=True,
+        help=(
+            "Tag to assign to the newly built container, e.g. "
+            "492475357299.dkr.ecr.us-west-2.amazonaws.com/xgb-ci.gpu:master"
+        ),
+    )
+    parser.add_argument(
+        "--build-arg",
+        type=str,
+        default=[],
+        action="append",
+        help=(
+            "Build-time variable(s) to be passed to `docker build`. Each variable "
+            "should be specified as a key-value pair in the form KEY=VALUE. "
+            "The variables should match the ARG instructions in the Dockerfile. "
+            "When passing multiple variables, specify --build-arg multiple times. "
+            "Example: --build-arg CUDA_VERSION_ARG=12.5 --build-arg RAPIDS_VERSION_ARG=24.10"
+        ),
+    )
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    parsed_args = parser.parse_args()
+    main(args=parsed_args)
---
+++ containers/docker_build.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+## Build a CI container and push it to AWS ECR (Elastic Container Registry).
+## This script provides a convenient wrapper for containers/docker_build.py.
+## Build-time variables (--build-arg) and container defintion are fetched from
+## containers/ci_container.yml.
+##
+## Note. This script takes in some inputs via environment variables.
+
+USAGE_DOC=$(
+cat <<-EOF
+Usage: containers/docker_build.sh [container_id]
+
+where [container_id] is used to fetch the container definition and build-time variables
+from containers/ci_container.yml.
+
+In addition, the following environment variables should be set.
+  - BRANCH_NAME:       Name of the current git branch or pull request (Required)
+  - GITHUB_SHA:        Git commit hash (Required)
+  - PUBLISH_CONTAINER: If set to 1, push the container to AWS ECR (Optional).
+                       This option requires appropriate AWS credentials to be
+                       configured.
+EOF
+)
+
+# Use AWS ECR (Elastic Container Registry) to host Docker containers.
+# Configure ECR to delete containers older than 30 days.
+ECR_AWS_ACCOUNT_ID="492475357299"
+ECR_AWS_REGION="us-west-2"
+ECR_URL="${ECR_AWS_ACCOUNT_ID}.dkr.ecr.${ECR_AWS_REGION}.amazonaws.com"
+ECR_LIFECYCLE_RULE=$(
+cat <<-EOF
+{
+   "rules": [
+       {
+           "rulePriority": 1,
+           "selection": {
+               "tagStatus": "any",
+               "countType": "sinceImagePushed",
+               "countUnit": "days",
+               "countNumber": 30
+           },
+           "action": {
+               "type": "expire"
+           }
+       }
+   ]
+}
+EOF
+)
+
+set -euo pipefail
+
+for arg in "BRANCH_NAME" "GITHUB_SHA"
+do
+  if [[ -z "${!arg:-}" ]]
+  then
+    echo -e "Error: $arg must be set.\n\n${USAGE_DOC}"
+    exit 1
+  fi
+done
+
+if [[ "$#" -lt 1 ]]
+then
+  echo "${USAGE_DOC}"
+  exit 2
+fi
+CONTAINER_ID="$1"
+
+# Fetch CONTAINER_DEF and BUILD_ARGS
+source <(containers/extract_build_args.sh ${CONTAINER_ID} | tee /dev/stderr) 2>&1
+
+if [[ "${PUBLISH_CONTAINER:-}" != "1" ]]   # Any value other than 1 is considered false
+then
+  PUBLISH_CONTAINER=0
+fi
+
+if [[ ${PUBLISH_CONTAINER} -eq 0 ]]
+then
+  echo "PUBLISH_CONTAINER not set; the container will not be published"
+else
+  echo "The container will be published at ${ECR_URL}"
+  # Login for Docker registry
+  echo "aws ecr get-login-password --region ${ECR_AWS_REGION} |" \
+       "docker login --username AWS --password-stdin ${ECR_URL}"
+  aws ecr get-login-password --region ${ECR_AWS_REGION} \
+    | docker login --username AWS --password-stdin ${ECR_URL}
+fi
+
+# Run Docker build
+set -x
+CONTAINER_TAG="${ECR_URL}/${CONTAINER_ID}:${GITHUB_SHA}"
+python3 containers/docker_build.py \
+  --container-def ${CONTAINER_DEF} \
+  --container-tag ${CONTAINER_TAG} \
+  ${BUILD_ARGS}
+set +x
+
+# Now push the new container to ECR
+if [[ ${PUBLISH_CONTAINER} -eq 1 ]]
+then
+    # Attempt to create Docker repository; it will fail if the repository already exists
+    echo "aws ecr create-repository --repository-name ${CONTAINER_ID} --region ${ECR_AWS_REGION}"
+    if aws ecr create-repository --repository-name ${CONTAINER_ID} --region ${ECR_AWS_REGION}
+    then
+      # Repository was created. Now set expiration policy
+      echo "aws ecr put-lifecycle-policy --repository-name ${CONTAINER_ID}" \
+           "--region ${ECR_AWS_REGION} --lifecycle-policy-text file:///dev/stdin"
+      echo "${ECR_LIFECYCLE_RULE}" | aws ecr put-lifecycle-policy --repository-name ${CONTAINER_ID} \
+        --region ${ECR_AWS_REGION} --lifecycle-policy-text file:///dev/stdin
+    fi
+
+    echo "docker push --quiet ${CONTAINER_TAG}"
+    if ! time docker push --quiet "${CONTAINER_TAG}"
+    then
+        echo "ERROR: could not update Docker cache ${CONTAINER_TAG}"
+        exit 1
+    fi
+
+    # Create another alias for the container using the branch name
+    CONTAINER_ALIAS="${ECR_URL}/${CONTAINER_ID}:${BRANCH_NAME}"
+    echo "docker tag ${CONTAINER_TAG} ${CONTAINER_ALIAS}"
+    docker tag ${CONTAINER_TAG} ${CONTAINER_ALIAS}
+    echo "docker push --quiet ${CONTAINER_ALIAS}"
+    docker push --quiet "${CONTAINER_ALIAS}"
+fi
