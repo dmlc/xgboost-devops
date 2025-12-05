5bd0bccdc86006dce745b645d2e58e24306cbd64
Hyunsu Cho <phcho@nvidia.com>
2025-12-05 12:38:42 -0800

    Install CUDA driver into VM image for ARM64

--- vm_images/linux-arm64/bootstrap.sh
+++ vm_images/linux-arm64/bootstrap.sh
@@ -31,6 +31,19 @@ sudo systemctl is-active --quiet docker.service || sudo systemctl start docker.s
 sudo systemctl is-enabled --quiet docker.service || sudo systemctl enable docker.service
 sleep 10  # Docker daemon takes time to come up after installing
 sudo docker info
+
+## Install NVIDIA Container Toolkit
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
+
+sleep 10
+sudo docker run --rm --gpus all ubuntu nvidia-smi
 sudo systemctl stop docker
 
 ## Install AWS CLI v2
