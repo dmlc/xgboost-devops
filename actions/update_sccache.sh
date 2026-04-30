89597eb1327f32aee2523dabc5d62de80e3263a4
Jiaming Yuan <jm.yuan@outlook.com>
2026-04-30 18:21:27 +0800

    check is updated.

--- actions/update_sccache.sh
+++ actions/update_sccache.sh
@@ -22,6 +22,24 @@ echo "LATEST_SCCACHE_VERSION = ${LATEST_SCCACHE_VERSION}"
 DIR_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 SCCACHE_ACTION="${DIR_PATH}/sccache/action.yml"
 
+get_sccache_action_version() {
+  sed -n -E "s/[[:space:]]*default:[[:space:]]*'(v[0-9]+\.[0-9]+\.[0-9]+-rapids\.[0-9]+)'/\1/p" \
+    "${SCCACHE_ACTION}"
+}
+
+CURRENT_SCCACHE_VERSION=$(get_sccache_action_version)
+
 sed -i -E \
   "s/(default:[[:space:]]*)'v[0-9]+\.[0-9]+\.[0-9]+-rapids\.[0-9]+'/\1'${LATEST_SCCACHE_VERSION}'/" \
   "${SCCACHE_ACTION}"
+
+UPDATED_SCCACHE_VERSION=$(get_sccache_action_version)
+
+if [[ "${UPDATED_SCCACHE_VERSION}" != "${LATEST_SCCACHE_VERSION}" ]]; then
+  echo "Failed to update ${SCCACHE_ACTION} to ${LATEST_SCCACHE_VERSION}." >&2
+  exit 1
+fi
+
+if [[ "${CURRENT_SCCACHE_VERSION}" == "${UPDATED_SCCACHE_VERSION}" ]]; then
+  echo "sccache is already up to date."
+fi
